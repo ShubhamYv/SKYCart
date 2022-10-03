b4c85336aed7467cab61832e01b8d69b097f89a2
Shubham <shubhamyadav32100@gmail.com>
2022-10-03 11:22:26 +0530

    some changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,100 +2,139 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import { Button } from "../styles/Button";
-import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaPhone } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi"
-import { ImWhatsapp } from "react-icons/im"
+import {
+  FaFacebook,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaPhone,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { ImWhatsapp } from "react-icons/im";
 
 const Footer = () => {
-    return (
-        <>
-            <Wrapper>
-                <section className="contact-short">
-                    <div className="grid grid-two-column">
-                        <div>
-                            <h3>Ready to get started?</h3>
-                            <h3>Talk to us today</h3>
-                        </div>
+  return (
+    <>
+      <Wrapper>
+        <section className="contact-short">
+          <div className="grid grid-two-column">
+            <div>
+              <h3>Ready to get started?</h3>
+              <h3>Talk to us today</h3>
+            </div>
 
-                        <div>
-                            <Button className="btn hireme-btn">
-                                <NavLink to="/"> Get Started </NavLink>
-                            </Button>
-                        </div>
-                    </div>
-                </section>
-                {/* footer section */}
+            <div>
+              <Button className="btn hireme-btn">
+                <NavLink to="/"> Get Started </NavLink>
+              </Button>
+            </div>
+          </div>
+        </section>
+        {/* footer section */}
 
-                <footer>
-                    <div className="container grid grid-four-column">
-                        <div className="footer-about">
-                            <h3>Shubham Yadav</h3>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
-                        </div>
-                        <div className="footer-subscribe">
-                            <h3>Send mail to get updates</h3>
-                            <form action="#">
-                                <input type="email" name="email" placeholder="YOUR E-MAIL" />
+        <footer>
+          <div className="container grid grid-four-column">
+            <div className="footer-about">
+              <h3>Shubham Yadav</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            </div>
+            <div className="footer-subscribe">
+              <h3>Send mail to get updates</h3>
+              <form action="#">
+                <input type="email" name="email" placeholder="YOUR E-MAIL" />
 
-                                <input type="submit" value="Send" />
-                            </form>
-                        </div>
-                        <div className="footer-social">
-                            <h3>Follow me</h3>
-                            <div className="footer-social--icons">
-                                <div>
-                                    <a href="https://github.com/ShubhamYv" target="_blank" rel="noreferrer">
-                                        <FaGithub className="icons" />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="https://www.linkedin.com/in/shubham-yadav-04a1b0200/" target="_blank" rel="noreferrer">
-                                        <FaLinkedin className="icons" />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="https://www.instagram.com/shubham.yv/" target="_blank" rel="noreferrer">
-                                        <FaInstagram className="icons" />
-                                    </a>
-                                </div>
-                                <div>
-                                    <a href="https://www.facebook.com/profile.php?id=100004831158656" target="_blank" rel="noreferrer">
-                                        <FaFacebook className="icons" />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="footer-contact">
-                            <h3>Contact Me</h3>
-                            <div className="footer-social--icons">
-                                <div>
-                                    <a href="tel: 9955883779"> <FaPhone className="icons" /> </a>
-                                </div>
-                                <div>
-                                    <a href="mailto:shubhamyadav32100@gmail.com"> <HiOutlineMail className="icons" /> </a>
-                                </div>
-                                <div>
-                                <a href="https://api.whatsapp.com/send?phone=9955883779"> <ImWhatsapp className="icons" /> </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <input type="submit" value="Send" />
+              </form>
+            </div>
+            <div className="footer-social">
+              <h3>Follow me</h3>
+              <div className="footer-social--icons">
+                <div>
+                  <a
+                    href="https://github.com/ShubhamYv"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaGithub className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="https://www.linkedin.com/in/shubham-yadav-04a1b0200/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaLinkedin className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="https://www.instagram.com/shubham.yv/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaInstagram className="icons" />
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="https://www.facebook.com/profile.php?id=100004831158656"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaFacebook className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="footer-contact">
+              <h3>Contact Me</h3>
+              <div className="footer-social--icons">
+                <div>
+                  <a href="tel: 9955883779" target="_blank" rel="noreferrer">
+                    {" "}
+                    <FaPhone className="icons" />{" "}
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="mailto:shubhamyadav32100@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <HiOutlineMail className="icons" />{" "}
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="https://api.whatsapp.com/send?phone=9955883779"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <ImWhatsapp className="icons" />{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
 
-                    <div className="footer-bottom--section">
-                        <hr />
-                        <div className="container grid grid-two-column ">
-                            <p>
-                                @{new Date().getFullYear()} Shubham Yadav. All Rights Reserved
-                            </p>
-                            <div>
-                                <p>PRIVACY POLICY</p>-<p>TERMS & CONDITIONS</p>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </Wrapper>
-        </>
-    );
+          <div className="footer-bottom--section">
+            <hr />
+            <div className="container grid grid-two-column ">
+              <p>
+                @{new Date().getFullYear()} Shubham Yadav. All Rights Reserved
+              </p>
+              <div>
+                <p>PRIVACY POLICY</p>-<p>TERMS & CONDITIONS</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </Wrapper>
+    </>
+  );
 };
 
 const Wrapper = styled.section`
