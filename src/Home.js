e4bfcdc2dc86a221a27d9cf71b2ffc45454e5f10
Shubham <shubhamyadav32100@gmail.com>
2022-10-07 20:51:04 +0530

    product added

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
+import FeatureProduct from "./components/FeatureProduct";
 import HeroSection from "./components/HeroSection";
 import Services from "./components/Services";
 import Trusted from "./components/Trusted";
 
 const Home = () => {
   const data = {
-    name: "SKYcart",
+    name: "thapa store",
   };
 
   return (
     <>
       <HeroSection myData={data} />
+      <FeatureProduct />
       <Services />
       <Trusted />
     </>
