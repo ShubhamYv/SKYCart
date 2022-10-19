8e9bae0cbf6769fa42605b215070b1741a738da9
Shubham <shubhamyadav32100@gmail.com>
2022-10-19 20:04:38 +0530

    adding list view and grid view

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,39 +1,26 @@
-import styled from "styled-components";
-import FilterSection from "./components/FilterSection";
-import ProductList from "./components/ProductList";
-import Sort from "./components/Sort";
-import { useFilterContext } from "./context/filter_context";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import FormatPrice from "../Helpers/FormatPrice";
 
-const Products = () => {
+const Product = (curElem) => {
+  const { id, name, image, price, category } = curElem;
   return (
-    <Wrapper>
-      <div className="container grid grid-filter-column">
-        <div>
-          <FilterSection />
-        </div>
+    <NavLink to={`/singleproduct/${id}`}>
+      <div className="card">
+        <figure>
+          <img src={image} alt={name} />
+          <figcaption className="caption">{category}</figcaption>
+        </figure>
 
-        <section className="product-view--sort">
-          <div className="sort-filter">
-            <Sort />
+        <div className="card-data">
+          <div className="card-data-flex">
+            <h3>{name}</h3>
+            <p className="card-data--price">{<FormatPrice price={price} />}</p>
           </div>
-          <div className="main-product">
-            <ProductList />
-          </div>
-        </section>
+        </div>
       </div>
-    </Wrapper>
+    </NavLink>
   );
 };
 
-const Wrapper = styled.section`
-  .grid-filter-column {
-    grid-template-columns: 0.2fr 1fr;
-  }
-  @media (max-width: ${({ theme }) => theme.media.mobile}) {
-    .grid-filter-column {
-      grid-template-columns: 1fr;
-    }
-  }
-`;
-
-export default Products;
+export default Product;
